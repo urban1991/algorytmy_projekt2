5978eadee1f15622f0586d45e778ed9e67fcbced
piotr.urbaniak <piotr.urbaniak.extern@sport1.de>
2025-01-15 12:41:31 +0100

    imp: add page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,94 @@
-import Image from "next/image";
+'use client';
+import React, { useState } from "react";
+import {
+    Container,
+    Box,
+    Paper, SxProps, Theme
+} from "@mui/material";
+
+import {isValidCoinAmount} from "@/utills/isValidCoinAmount";
+import {getMinimalCoinDistribution} from "@/utills/getMinCoinDistribution";
+import {Podsumowanie} from "@/components/Podsumowanie";
+import {Header} from "@/components/Header";
+import {Opis} from "@/components/Opis";
+import {Wynik} from "@/components/Wynik";
+import {Snippets} from "@/components/Snippets";
+import {InputForm} from "@/components/InputForm";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [inputValue, setInputValue] = useState("");
+    const [isValid, setIsValid] = useState(true);
+    const [warningMsg, setWarningMsg] = useState("");
+    const [result, setResult] = useState<Record<string, number> | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const val = e.target.value;
+        setInputValue(val);
+
+        if (!val) {
+            setIsValid(true);
+            setWarningMsg("");
+            setResult(null);
+            return;
+        }
+
+        if (isValidCoinAmount(val)) {
+            setIsValid(true);
+            setWarningMsg("");
+        } else {
+            setIsValid(false);
+            setWarningMsg("Dozwolone są tylko liczby całkowite lub format x.25, x.5, x.75");
+            setResult(null);
+        }
+    };
+
+    const handleCalculate = () => {
+        if (!inputValue || !isValid) return;
+
+        console.log(`inputValue: `, Number(inputValue))
+
+        const distribution = getMinimalCoinDistribution(Number(inputValue));
+        setResult(distribution);
+    };
+
+
+    const paperProps:  SxProps<Theme> = {
+        p: 3,
+        backgroundColor: "#fafafa",
+        borderRadius: 2,
+        boxShadow: 3
+    }
+
+    const boxProps: SxProps<Theme> = {
+        background: "linear-gradient(135deg, #e0f2f1, #fff)",
+        minHeight: "100vh",
+        py: 6,
+    }
+
+    return (
+        <Box sx={boxProps}>
+            <Container maxWidth="md">
+                <Header />
+                <Opis />
+
+                <Box mb={4}>
+                    <Paper elevation={2} sx={paperProps}>
+                        <InputForm
+                            inputValue={inputValue}
+                            handleCalculate={handleCalculate}
+                            handleChange={handleChange}
+                            isValid={isValid}
+                            warningMsg={warningMsg}
+                        />
+
+                        <Wynik result={result} inputValue={inputValue} />
+                    </Paper>
+                </Box>
+
+                <Snippets />
+
+                <Podsumowanie />
+          </Container>
+      </Box>
+    );
 }
